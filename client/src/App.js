37a0c9981318074346346e370e225b71692541eb
Christopher Bailey <seabayley@outlook.com>
2019-10-04 10:14:56 -0500

    Added api call

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      apiUrl: 'http://localhost:5000/api/players',
+      playerData: null,
+    }
+  }
+
+  componentDidMount() {
+    this.getPlayerData()
+  }
+
+  getPlayerData() {
+    let url = (this.state.apiUrl)
+    axios.get(url)
+      .then(res => {
+        this.setState({ playerData: res.data })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    return !this.state.playerData ? <p>Loading...</p> :
+      (
+        <div>
+
+        </div>
+      )
+  }
 }
 
 export default App;
